afa96de0fa3248c74757799ea67a6a7cb2cd3f67
Sh1Nek0 <annypodkaluk@gmail.com>
2026-04-08 20:29:48 +0800

    added accordion

---
+++ mersedes/js/accordion.js
@@ -0,0 +1,20 @@
+const lists = document.querySelectorAll('.feature-sub' )
+const btns = document.querySelectorAll('.feature__link')
+
+btns.forEach((btnItem, index) => {
+    btnItem.addEventListener('click', () => {
+        btns. forEach((btnItem) => {
+            btnItem.classList.remove('feature_link_active')
+        })
+
+    btnItem.classList.add('feature__link_active')
+
+    lists.forEach((listItem) => {
+        listItem.classList.add('hidden')
+    })
+
+    lists[index].classList.remove('hidden')
+
+    })
+
+})
